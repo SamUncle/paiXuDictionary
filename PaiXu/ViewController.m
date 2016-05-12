487da1be340682fd300e92136d2df0a95e613aff
xiaoming <17717926231@163.com>
2016-05-12 21:51:19 +0800

    加了注释

--- PaiXu/ViewController.m
+++ PaiXu/ViewController.m
@@ -17,13 +17,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //思路：首先将数组中的字符串取出，截取字符串，取出数字，用冒泡排序法排序好后再拼接字符串，然后放到字典（自定义的有序的字典）。这样就完成了对一个数组中的字符串排序到有序字典中。    
     NSMutableArray * array = [NSMutableArray arrayWithObjects:@"b_22",@"b_34",@"b_78",@"b_6",@"b_8",@"b_23", nil];
+    //创建一个装数字的字典
     NSMutableArray * news = [NSMutableArray array];
     for (int i = 0; i <[array count]; i++) {
        NSString * str = [array objectAtIndex:i];
        NSString * newstr = [[str componentsSeparatedByString:@"b_"] lastObject];
        [news addObject:newstr];
     }
+    //下面使用冒泡排序法排序
     for (int i = 0; i<[news count]; i ++) {
         for (int j = i+1; j<[news count]; j++) {
             int a = [[news objectAtIndex:i] intValue];
@@ -34,6 +37,7 @@
             }
         }
     }
+    //创建一个自定义的有序字典
     SSDictionary * dic = [[SSDictionary alloc]initWithCapacity:[news count]];
     for (int i = 0; i < [news count]; i++) {
         NSLog(@"%@",[news objectAtIndex:i]);
@@ -42,6 +46,7 @@
         NSLog(@"%@",newStr);
         [dic setObject:newStr forKey:[NSNumber numberWithInt:i]];
     }
+    //打印需要的字典
     NSLog(@"### %@",dic);
     
     
