f4b25da8f92501b29d00499ec36957682b691549
xiaoming <17717926231@163.com>
2016-05-13 13:23:33 +0800

    添加拼接有序字符串

--- PaiXu/ViewController.m
+++ PaiXu/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     //思路：首先将数组中的字符串取出，截取字符串，取出数字，用冒泡排序法排序好后再拼接字符串，然后放到字典（自定义的有序的字典）。这样就完成了对一个数组中的字符串排序到有序字典中。    
-    NSMutableArray * array = [NSMutableArray arrayWithObjects:@"b_22",@"b_34",@"b_78",@"b_6",@"b_8",@"b_23", nil];
+    NSMutableArray * array = [NSMutableArray arrayWithObjects:@"123b_22",@"123b_34",@"123b_78",@"123b_6",@"123b_8",@"123b_23", nil];
     //创建一个装数字的字典
     NSMutableArray * news = [NSMutableArray array];
     for (int i = 0; i <[array count]; i++) {
@@ -26,6 +26,7 @@
        NSString * newstr = [[str componentsSeparatedByString:@"b_"] lastObject];
        [news addObject:newstr];
     }
+    NSLog(@"ne %@",[[[array firstObject] componentsSeparatedByString:@"b_"] firstObject] );
     //下面使用冒泡排序法排序
     for (int i = 0; i<[news count]; i ++) {
         for (int j = i+1; j<[news count]; j++) {
@@ -39,16 +40,23 @@
     }
     //创建一个自定义的有序字典
     SSDictionary * dic = [[SSDictionary alloc]initWithCapacity:[news count]];
+    NSMutableArray * youxus = [NSMutableArray array];
     for (int i = 0; i < [news count]; i++) {
-        NSLog(@"%@",[news objectAtIndex:i]);
+        //NSLog(@"%@",[news objectAtIndex:i]);
         NSString * str = [news objectAtIndex:i];
-        NSString * newStr = [NSString stringWithFormat:@"b_%@",str];
-        NSLog(@"%@",newStr);
+        
+        NSString * newStr = [NSString stringWithFormat:@"%@b_%@",[[[array firstObject] componentsSeparatedByString:@"b_"] firstObject],str];
+        NSLog(@"newStr %@",newStr);
+        //添加到有序数组里
+        [youxus addObject:newStr];
+        
         [dic setObject:newStr forKey:[NSNumber numberWithInt:i]];
     }
+   NSString * xuStr = [youxus componentsJoinedByString:@","];
+    //打印拼接好的字符串
+    NSLog(@"xuStr %@",xuStr);
     //打印需要的字典
     NSLog(@"### %@",dic);
-    
     
 }
 
